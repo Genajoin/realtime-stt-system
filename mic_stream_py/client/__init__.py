656671b4f5a2c5716de811c72290a0cd07855c6f
Evgeny Istomin <gena@regimov.net>
2025-09-25 22:02:44 +0200

    docs: обновление readme

--- mic_stream_py/client/__init__.py
+++ mic_stream_py/client/__init__.py
@@ -3,8 +3,6 @@ Clients package for Mic Stream Py
 
 Содержит различные клиенты для STT системы:
 - minimal_editor: Минималистичный терминальный редактор
-- gui_editor: GUI редактор на tkinter
-- gui_launcher: Автозапуск GUI редактора
 """
 
 __all__ = []
